0d29076104a36426f0d3c120002959b340091432
Prabodha Jayawardena <prabodha@dev.bukflights.com>
2022-06-04 11:49:31 +0530

    🚀  created simple app with shallow merger and access previous state from setState and get callback

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super();
+		this.state = {
+			name: {
+				firstName: 'Prabodha',
+				lastName: 'Jaye',
+			},
+
+			company: 'eyepax',
+		};
+	}
+
+	changeName() {
+		// {
+		// 	name: {
+		//     firstName:'Prabodha',
+		//     lastName:'Jaye',
+		//   },
+		// 	company: 'eyepax',
+		// }
+		//this is shallow merger that means react doesnt care about what the value was before and how complex that is
+
+		//if we change the name like below we no longer has access to firstName and lastName becase it doesnt care about the previus value
+		// this.setState({ name: 'Changed name' });
+
+		this.setState(
+			(state, props) => {
+				return { name: { firstName: 'Changed', lastName: 'Name' }, company: 'eyepax' };
+			},
+			() => {
+				console.log(this.state);
+			}
+		);
+	}
+	render() {
+		return (
+			<div className='App'>
+				<header className='App-header'>
+					<img src={logo} className='App-logo' alt='logo' />
+					<p>
+						Hello {this.state.name.firstName}, hi i work at {this.state.company}
+					</p>
+					<button onClick={this.changeName.bind(this)}>Change Name</button>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
